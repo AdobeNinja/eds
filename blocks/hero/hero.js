cc91f03ad889bae4747e5238acdf456762e9c078
AdobeNinja <59413815+AdobeNinja@users.noreply.github.com>
2024-07-16 09:34:58 +0200

    Update hero.js
    back to original

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,2 +1,1 @@
-<!-- Preload the LCP image with a high fetchpriority so it starts loading with the stylesheet. -->
-<link rel="preload" fetchpriority="high" as="image" href="https://main--eds--adobeninja.hlx.live/media_1bee04d9abe2b1b2fc9fbe5f4c590b0d0dbbd2262.jpeg?width=2000&format=webply&optimize=medium.webp" type="image/webp">
+
